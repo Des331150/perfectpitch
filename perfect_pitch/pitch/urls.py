95b62872bb23ddd824e240dd7c4a5389f52bc424
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-07-30 17:57:30 +0000

    finished with the sign up section

--- perfect_pitch/pitch/urls.py
+++ perfect_pitch/pitch/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "pitch"
 
-urlpatterns = [path("", views.login, name="login")]
+urlpatterns = [
+    path("", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+]
