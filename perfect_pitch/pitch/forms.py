f28f6a95a2a15558d5ad461dd6c237db61aeed38
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-08-07 15:52:32 +0000

    made a few edits in the user registration form

--- perfect_pitch/pitch/forms.py
+++ perfect_pitch/pitch/forms.py
@@ -9,15 +9,6 @@ class UserRegistrationForm(UserCreationForm):
         model = CustomUser
         fields = ["name", "email", "password1", "password2"]
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
-
-        if password1 != password2:
-            raise forms.ValidationError("Passwords do not match.")
-        return cleaned_data
-
 
 class UserLoginForm(forms.Form):
     email = forms.EmailField(
